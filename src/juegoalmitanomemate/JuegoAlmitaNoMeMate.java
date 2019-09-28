34ddeb9fa68205e629f8657e2cf99006dd579517
Kvothes <bearexe51@gmail.com>
2019-09-28 11:43:58 -0500

    Comenzamos

---
+++ src/juegoalmitanomemate/JuegoAlmitaNoMeMate.java
@@ -0,0 +1,13 @@
+
+package juegoalmitanomemate;
+
+
+public class JuegoAlmitaNoMeMate {
+
+
+    public static void main(String[] args) {
+        Menu men = new Menu();
+        men.setVisible(true);
+    }
+    
+}
